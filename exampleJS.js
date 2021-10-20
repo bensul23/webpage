e1d90f63895e0c740ade4c130c47a8f2334364be
bensul <bensul23@bergen.org>
2021-10-20 10:46:19 -0400

    vid update

---
+++ exampleJS.js
@@ -0,0 +1,11 @@
+//alert("wasssahh");
+let x = prompt("who's joe?", "joe mama");
+if(x==null || x=="")
+    alert("Joe mama is fat (you can't outsmart me)")
+else
+    alert(x+" is fat LOOOOL BAITED");
+    alert("illusion of choice")
+// print(2+"2")
+// print("2"+"2")
+// print(2+2)
+
